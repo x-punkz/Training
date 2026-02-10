9c724707b297d646b71d9f375f500c716e01ac04
x-punkz <r2sskateboard@gmail.com>
2026-02-10 11:42:07 -0300

    exercicios

---
+++ lv1/first_word.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int main (int argc, char **argv)
+{
+    int i;
+
+    i = 0;
+    if (argc == 2)
+    {
+        while (argv[1][i])
+        {
+            if (argv[1][i] >= 33 || argv[1][i] <= 126)
+                write(1, &argv[1][i], 1);
+            if (argv[1][i + 1] <= 33 || argv[1][i + 1] >= 126)
+                /*return (0)*/break ;
+			i++;
+        }
+		write (1, "\n", 1);
+    }
+    else
+        write(1, "\n", 1);
+}
